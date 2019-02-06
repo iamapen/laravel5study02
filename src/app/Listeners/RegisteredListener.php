e95c27c32b2206eb7922b694018146c4740df0c9
pen <iamapen@studiopoppy.com>
2019-02-06 15:04:08 +0900

    1-3-7 会員登録イベントでメール送信

--- src/app/Listeners/RegisteredListener.php
+++ src/app/Listeners/RegisteredListener.php
@@ -2,30 +2,43 @@
 
 namespace App\Listeners;
 
+use App\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailer;
 
+/**
+ * 会員登録時に実行されるリスナ
+ */
 class RegisteredListener
 {
+    /** @var Mailer */
+    private $mailer;
+    /** @var User */
+    private $eloquent;
+
     /**
      * Create the event listener.
-     *
-     * @return void
+     * @param Mailer $mailer
+     * @param User $eloquent
      */
-    public function __construct()
+    public function __construct(Mailer $mailer, User $eloquent)
     {
-        //
+        $this->mailer = $mailer;
+        $this->eloquent = $eloquent;
     }
 
     /**
      * Handle the event.
      *
-     * @param  Registered  $event
+     * @param  Registered $event
      * @return void
      */
     public function handle(Registered $event)
     {
-        //
+        $user = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('会員登録完了しました', function ($msg) use ($user) {
+            /* @var \Illuminate\Mail\Message $msg */
+            $msg->subject('会員登録メール')->to($user->email);
+        });
     }
 }
