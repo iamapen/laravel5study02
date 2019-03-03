426c124b4dde17938a8c0e133aa09de14fca66b2
pen <iamapen@studiopoppy.com>
2019-03-03 20:29:12 +0900

    3.2.3.6 サービスからはリポジトリを使う
    アクション→ドメインロジック→リポジトリ→データソース

--- src/app/Services/UserPurchaseService.php
+++ src/app/Services/UserPurchaseService.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Purchase;
+use App\Repositories\UserRepositoryInterface;
 use App\User;
 
 class UserPurchaseService
 {
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @param int $identifier
      * @return User
      */
     public function retrievePurchase(int $identifier): User
     {
-        $user = User::find($identifier);
-        $user->purchase = Purchase::findAllBy($user->id);
+        $arrUser = $this->userRepository->find($identifier);
         // DBから取得した値を使った処理など
-
+        $user = new User($arrUser);
         return $user;
     }
 }
