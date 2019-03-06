4b5222aadb50de62fae36129e3bf6dcfc75675d3
pen <iamapen@studiopoppy.com>
2019-03-06 14:10:01 +0900

    4.2.3.2 コントローラでvalidatorクラスを使ったバリデーション処理

--- src/app/Http/Actions/Article/ArticleAddAction.php
+++ src/app/Http/Actions/Article/ArticleAddAction.php
@@ -5,21 +5,35 @@ namespace App\Http\Actions\Article;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Validation\Factory as ValidatorFactory;
+use Illuminate\Validation\ValidationException;
 
 /**
  * 記事追加
  */
 final class ArticleAddAction extends \App\Http\Controllers\Controller
 {
-    public function __invoke(Request $request): Response
-    {
+    /**
+     * @param Request $request
+     * @param ValidatorFactory $validatorFactory
+     * @return Response
+     */
+    public function __invoke(
+        Request $request,
+        ValidatorFactory $validatorFactory
+    ): Response {
+        $inputs = $request->all();
         $rules = [
-            'name' => ['required', 'max:10', 'unique:users',],
-            'email' => ['required', 'email', 'max:255', 'unique:users,email',],
+            'name' => ['required', 'max:10', 'unique:users', ],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email', ],
             'age' => 'integer',
         ];
 
-        $this->validate($request, $rules);
+        $validator = $validatorFactory->make($inputs, $rules);
+        if ($validator->fails()) {
+            // バリデーションエラーの場合の処理
+            throw new ValidationException($validator);
+        }
 
         // 登録処理
         //$this->domain->activate(
