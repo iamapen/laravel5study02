b614205b0d0c8ba6b353489f596ed3f376bff854
pen <iamapen@studiopoppy.com>
2019-03-06 20:19:39 +0900

    4-4 ミドルウェア

--- src/app/Http/Middleware/HeaderDumper.php
+++ src/app/Http/Middleware/HeaderDumper.php
@@ -3,18 +3,48 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Psr\Log\LoggerInterface;
 
+/**
+ * リクエスト/レスポンスヘッダをログに記録するミドルウェア
+ * @package App\Http\Middleware
+ */
 class HeaderDumper
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if ($request instanceof Request) {
+            // loggerを触る場合
+            $this->logger->info('request', [
+                'header' => (string)$request->headers,
+            ]);
+            // ヘルパ関数を使う場合
+            //info('request', ['header' => (string)$request->headers]);
+        }
+
+        $response = $next($request);
+        if ($response instanceof Response) {
+            $this->logger->info('response', [
+                'header' => (string)$response->headers,
+            ]);
+        }
+
+        return $response;
     }
 }
