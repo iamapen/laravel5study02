785d89b2724097cbbfa0c831bb976c0b0da44554
pen <iamapen@studiopoppy.com>
2019-04-10 12:55:27 +0900

    add accessor tests

---
+++ src/tests/Unit/AuthorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Author;
+
+class AuthorTest extends TestCase
+{
+
+    function testAccessor()
+    {
+        $sut = new Author();
+
+        $sut->kana = 'カナ';
+        $this->assertSame('カナ', $sut->kana);
+
+        $sut->kana = 'ｶﾅ';
+        $this->assertSame('ｶﾅ', $sut->kana);
+    }
+}
