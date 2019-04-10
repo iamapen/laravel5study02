7bfe4113ac35afbbe6c437ce832c049b0f0507d2
pen <iamapen@studiopoppy.com>
2019-04-10 10:12:03 +0900

    5.3.2.5 編集不可なカラムをブラックリストで指定

--- src/app/Author.php
+++ src/app/Author.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    // Mass Assignmentによる編集可能なカラムのホワイトリスト指定
-    protected $fillable = ['name', 'kana'];
+    // Mass Assignmentによる編集可能なカラムのブラックリスト指定
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 }
