fead80eac68a46b1ca1fc42b78d16014f74d6f39
pen <iamapen@studiopoppy.com>
2019-04-10 10:22:08 +0900

    5.3.2.1 紐づけるテーブル名を指定できる

--- src/app/Author.php
+++ src/app/Author.php
@@ -8,4 +8,7 @@ class Author extends Model
 {
     // Mass Assignmentによる編集可能なカラムのブラックリスト指定
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    // テーブル名を指定できる
+    protected $table = 'authors';
 }
