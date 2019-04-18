787f270b3c39a7fe39a2483d1d3e583ec7f3b041
pen <iamapen@studiopoppy.com>
2019-04-18 22:11:19 +0900

    5.5.2.7 Favoriteモデルの実装

--- src/app/DataProvider/Eloquent/Favorite.php
+++ src/app/DataProvider/Eloquent/Favorite.php
@@ -1,10 +1,19 @@
 <?php
+declare(strict_types=1);
 
 namespace App\DataProvider\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 「いいね」のRDBアクセス
+ * @package App\DataProvider\Eloquent
+ */
 class Favorite extends Model
 {
-    //
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'created_at',
+    ];
 }
---
+++ src/app/Services/FavoriteService.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pen
+ * Date: 2019/04/18
+ * Time: 22:11
+ */
