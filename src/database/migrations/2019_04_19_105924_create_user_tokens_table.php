c19d8ac69bd5b7a3c9c4015679ca408b94362e73
pen <iamapen@studiopoppy.com>
2019-04-19 20:03:56 +0900

    6.2.1.3 user_tokensテーブル作成
    php artisan migrate

--- src/database/migrations/2019_04_19_105924_create_user_tokens_table.php
+++ src/database/migrations/2019_04_19_105924_create_user_tokens_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
 
 class CreateUserTokensTable extends Migration
 {
@@ -14,8 +15,12 @@ class CreateUserTokensTable extends Migration
     public function up()
     {
         Schema::create('user_tokens', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->integer('user_id', false, true);
+            $table->string('api_token', 60)->unique();
+            $table->timestamp('created_at')
+                ->default(new Expression('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
