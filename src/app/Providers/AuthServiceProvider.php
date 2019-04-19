3d4dd154821a54793aef2a41b43ab527be8313db
pen <iamapen@studiopoppy.com>
2019-04-19 22:18:43 +0900

    6.2.3.5 実装した認証プロバイダの登録

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Auth\UserTokenProvider;
+use App\DataProvider\Database\UserToken;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -28,12 +31,20 @@ class AuthServiceProvider extends ServiceProvider
         // 独自認証ドライバの登録
         $this->app['auth']->provider(
             'cache_eloquent',
-            function (\Illuminate\Contracts\Foundation\Application $app, array $config) {
+            function (Application $app, array $config) {
                 return new \App\Auth\CacheUserProvider(
                     $app['hash'],
                     $config['model'],
                     $app['cache']->driver()
                 );
+            }
+        );
+
+        // 独自認証ドライバを user_token として登録
+        $this->app['auth']->provider(
+            'user_token',
+            function (Application $app) {
+                return new UserTokenProvider(new UserToken($app['db']));
             }
         );
     }
