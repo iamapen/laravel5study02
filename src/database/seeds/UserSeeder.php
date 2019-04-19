d8e32cbaea87065015145ef6df79fe11cfb88a21
pen <iamapen@studiopoppy.com>
2019-04-19 20:52:43 +0900

    6-2-2 Seederを用いてレコード作成
    composer dumpautoload
    php artisan db:seed

--- src/database/seeds/UserSeeder.php
+++ src/database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Contracts\Hashing\Hasher;
 
 class UserSeeder extends Seeder
 {
@@ -9,8 +11,19 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(DatabaseManager $manager, Hasher $hasher)
     {
-        //
+        $userId = $manager->table('users')
+            ->insertGetId([
+                'name' => 'laravel user',
+                'email' => 'mail@example.com',
+                'password' => $hasher->make('password'),
+                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            ]);
+        $manager->table('user_tokens')
+            ->insert([
+                'user_id' => $userId,
+                'api_token' => \Illuminate\Support\Str::random(60),
+            ]);
     }
 }
