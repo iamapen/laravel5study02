31069a894e62ecb730a6d93042287b8239b02c87
pen <iamapen@studiopoppy.com>
2019-04-24 16:42:59 +0900

    6.3.2.1 JWTSubjectインタフェイスの実装

--- src/app/User.php
+++ src/app/User.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -26,4 +27,24 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * ユーザを特定できる一意な値を返す
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * JWTで利用するクレーム情報。
+     *
+     * 追加したいクレーム情報があれば配列で指定する。
+     * @return array|void
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
