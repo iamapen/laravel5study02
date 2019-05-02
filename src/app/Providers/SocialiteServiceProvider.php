ef567cc9e8d609093dcf983c01d26d4b6eefc967
pen <iamapen@studiopoppy.com>
2019-05-02 19:03:35 +0900

    6.4.4.4 Socialiteを拡張してドライバを追加

---
+++ src/app/Providers/SocialiteServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Foundation\Socialite\AmazonProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Socialite\Contracts\Factory;
+use Laravel\Socialite\SocialiteManager;
+
+class SocialiteServiceProvider extends ServiceProvider
+{
+    /**
+     * Socialiteのサービスプロバイダは遅延登録なのでboot()で
+     * @param Factory|SocialiteManager $factory
+     */
+    public function boot(Factory $factory)
+    {
+        $factory->extend('amazon', function (Application $app) use ($factory) {
+            return $factory->buildProvider(
+                AmazonProvider::class,
+                $app['config']['services.amazon']
+            );
+        });
+    }
+}
