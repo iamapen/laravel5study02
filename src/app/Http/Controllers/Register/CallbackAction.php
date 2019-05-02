7a2dffd75b6a9409ea19d05849b7788280d3c46d
pen <iamapen@studiopoppy.com>
2019-05-02 17:56:18 +0900

    6.4.3.1 通信内容をログとして出力する例

--- src/app/Http/Controllers/Register/CallbackAction.php
+++ src/app/Http/Controllers/Register/CallbackAction.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers\Register;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 use Illuminate\Auth\AuthManager;
 use Laravel\Socialite\Contracts\Factory;
+use Psr\Log\LoggerInterface;
 
 /**
  * Socialiteを利用したユーザ作成とログイン
@@ -16,10 +21,25 @@ class CallbackAction extends Controller
 {
     public function __invoke(
         Factory $factory,
-        AuthManager $authManager
+        AuthManager $authManager,
+        LoggerInterface $logger
     ) {
+        /* @var $driver \Laravel\Socialite\Two\GithubProvider */
+        $driver = $factory->driver('github');
+        // 通信内容をログとして出力させる例
+        $driver->setHttpClient(
+            new Client([
+                'handler' => tap(
+                    HandlerStack::create(),
+                    function (HandlerStack $stack) use ($logger) {
+                        $stack->push(Middleware::log($logger, new MessageFormatter()));
+                    }
+                )
+            ])
+        );
+
         // socialite経由で外部サービスのユーザ情報を取得
-        $user = $factory->driver('github')->user();
+        $user = $driver->user();
         // ユーザ作成＆ログイン
         $authManager->guard()->login(
             User::firstOrCreate([
