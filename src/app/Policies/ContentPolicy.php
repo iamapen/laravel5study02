5a374fedd9da24f6650bb48393a863ccc929b6ff
pen <iamapen@studiopoppy.com>
2019-05-02 21:19:03 +0900

    6.5.2.7 Eloquentモデルが記述されたメソッドを含むポリシクラスの作成
    php artisan make:policy ContentPolicy --model=Content

--- src/app/Policies/ContentPolicy.php
+++ src/app/Policies/ContentPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\User;
+use App\Content;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ContentPolicy
@@ -10,11 +11,48 @@ class ContentPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the content.
      *
-     * @return void
+     * @param  \App\User  $user
+     * @param  \App\Content  $content
+     * @return mixed
      */
-    public function __construct()
+    public function view(User $user, Content $content)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create contents.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the content.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Content  $content
+     * @return mixed
+     */
+    public function update(User $user, Content $content)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the content.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Content  $content
+     * @return mixed
+     */
+    public function delete(User $user, Content $content)
     {
         //
     }
